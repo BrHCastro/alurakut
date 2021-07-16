4bb632822bd432539a1b6c0536ee0831eeee99ae
brhcastro <hendecastro@gmail.com>
2021-07-15 21:27:37 -0300

    Criação de formulário para cadastro comunidades, hook e useState

--- README.md
+++ README.md
@@ -7,12 +7,22 @@
 ## Conteúdo detalhado desta aula:
 
 - [x] Iniciaremos um projeto com Create Next App;
-  ````
+  ```
     npx create-next-app --example with-styled-components
     # ou
     yarn create next-app --example with-styled-components
-  ````
+  ```
 - [x] Criaremos components com React usando styled-components;
 - [x] Organizaremos as pastas do nosso projeto;
 - [x] Passaremos propriedades para components;
 - [x] Faremos deploy do seu Alurakut na Vercel.
+
+> ## Aula 2: React, State e o primeiro Formulário
+<br/>
+
+## Conteúdo detalhado dessa aula
+
+- [x] Entender melhor o que é um SPA;
+- [x] Conhecer o hook useEffect;
+- [x] Criação do formulário para cadastrar comunidade;
+- [x] E sempre faremos deploy do seu Alurakut na Vercel.
--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,45 @@
 import MainGrid from '../src/components/MainGrid'
 import Box from '../src/components/Box'
-import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AluraCommons'
+import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet } from '../src/lib/AluraCommons'
 import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations'
+import React, { useState } from 'react'
 
 function ProfileSidebar(props) {
   return (
-    <Box>
-      <img src={`https://github.com/${props.profileImg}.png`} style={{ borderRadius: '8px' }} />
+    <Box as="aside">
+      <img src={`https://github.com/${props.githubUser}.png`} style={{ borderRadius: '8px' }} />
+      <hr/>
+
+      <p>
+        <a className="boxLink" href={`https://github.com/${props.githubUser}`}>
+          @{props.githubUser}
+        </a>
+      </p>
+
+      <hr/>
+      <AlurakutProfileSidebarMenuDefault/>
     </Box>
   )
 }
 
 export default function Home() {
-  const githubUser = 'brhcastro';
+  const [communities, setCommunities] = useState([{id: 1, title: 'Eu odeio acordar cedo', img: 'https://img10.orkut.br.com/community/b62636a928d479ea6b865953c0ee1010.png'}])
+  const githubUser = 'brhcastro'
   const peopleFavorites = [
     'juunegreiros',
     'omariosouto',
     'peas',
     'rafaballerini',
     'marcobrunodev',
-    'felipefialho'
+    'felipefialho',
+    'guilhermesilveira'
   ]
   return (
     <>
     <AlurakutMenu githubUser={ githubUser }/>
     <MainGrid>
       <div className="profileArea" style={{ gridArea: 'profileArea' }}>
-        <ProfileSidebar profileImg={githubUser}/>
+        <ProfileSidebar githubUser={githubUser}/>
       </div>
       <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
         <Box>
@@ -35,17 +48,76 @@ export default function Home() {
           </h1>
           <OrkutNostalgicIconSet/>
         </Box>
+        <Box>
+          <h2 className="subTitle">O que você deseja fazer?</h2>
+          <form onSubmit={(e) => {
+            e.preventDefault();
+            const dataForm = new FormData(e.target)
+
+            const title = dataForm.get('title')
+            const image = dataForm.get('image')
+
+            if (title.length === 0 || title === '' || title === ' ') {
+              alert('Por favor, insira um título para a sua comunidade.')
+            } else if (image.length === 0 || image === '' || image === ' ') {
+              alert('Por favor, insira uma URL com a imagem para a sua comunidade.')
+            } else {
+
+              const community = {id: new Date().toISOString(), title: title, img: image}
+
+              const communitiesUpdated = [...communities, community]
+              setCommunities(communitiesUpdated)
+            }
+          }}>
+            <div>
+              <input
+                type="text" 
+                name="title" 
+                area-label="Qual vai ser o nome da sua comunidade?" 
+                placeholder="Qual vai ser o nome da sua comunidade?"
+              />
+            </div>
+            <div>
+              <input
+                type="text" 
+                name="image" 
+                area-label="Coloque uma URL para usarmos de image." 
+                placeholder="Coloque uma URL para usarmos de capa."
+              />
+            </div>
+            <button>Criar comunidade</button>
+          </form>
+        </Box>
       </div>
       <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+        <ProfileRelationsBoxWrapper>
+        <h2 className="smallTitle">
+            Comunidade ({communities.length})
+        </h2>
+        <ul>
+          { communities.slice(0,6).map((community) => {
+            return (
+              <li key={community.id}>
+                <a href={`/community/${community.title}`}>
+                <img src={community.img} />
+                <span>{community.title}</span>
+              </a>
+              </li>
+            )
+          }) }
+          </ul>
+          <hr/>
+          <a className="boxLink" href="#">Ver mais</a>
+        </ProfileRelationsBoxWrapper>
         <ProfileRelationsBoxWrapper>
           <h2 className="smallTitle">
             Pessoas da comunidade ({peopleFavorites.length})
           </h2>
           <ul>
-          { peopleFavorites.map((person) => {
+          { peopleFavorites.slice(0,6).map((person) => {
             return (
-              <li>
-                <a href={`/users/${person}`} key={person}>
+              <li key={person}>
+                <a href={`/users/${person}`}>
                 <img src={`https://github.com/${person}.png`} />
                 <span>{person}</span>
               </a>
@@ -53,6 +125,8 @@ export default function Home() {
             )
           }) }
           </ul>
+          <hr/>
+          <a className="boxLink" href="#">Ver mais</a>
         </ProfileRelationsBoxWrapper>
       </div>
     </MainGrid>
